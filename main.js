5462d02dd970016c08227249f7d832a647643c44
nicoAiz <nicogu.safe@gmail.com>
2025-05-01 19:14:03 -0300

    Fixed window's title

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Test window
 const wins = [
-	createWindow('Test JS Window.png', '🤓', `
+	createWindow('Test JS Window', '🤓', `
 		<h1>Another window</h1>
 		<p>This window was created with JavaScript.</p>
 		<br>
